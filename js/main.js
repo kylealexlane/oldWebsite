fc3037981824eca7f8c11c42836cd1aecd5c3990
Kyle Lane <kyle_lane@Kyles-MacBook-Pro.local>
2017-09-26 19:22:54 -0400

    Updated to Clean Design

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-jQuery(document).ready(function($) {  
-
-// site preloader -- also uncomment the div in the header and the css style for #preloader
-$(window).load(function(){
-	$('#preloader').fadeOut('slow',function(){$(this).remove();});
-});
-
-});
+$(function(){
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop(); // how many pixels you've scrolled
+        var os = $('#about').offset().top; // pixels to the top of div1
+        var navHeight = $('#mainNav').height();
+        // change the color. either by adding a class or setting a css property
+        if(scroll > os - navHeight){
+            $('#mainNav').addClass('darkNavBackground');
+        }
+        if(scroll < os - navHeight ){
+            $('#mainNav').removeClass('darkNavBackground');
+        }
+    });
+}); 
