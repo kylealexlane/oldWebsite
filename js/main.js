8b26b711c17a1cc68f9fc0ed787a85702ae98e4f
Kyle Lane <kyle_lane@Kyles-MacBook-Pro.local>
2017-09-27 17:47:50 -0400

    pre-loading and smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+//CHANGING THE NAVBAR BACKGROUND COLOR
 $(function(){
     $(window).scroll(function() {
         var scroll = $(window).scrollTop(); // how many pixels you've scrolled
@@ -14,3 +14,36 @@ $(function(){
     });
 }); 
   
+
+//PRELOADER ANIMATION
+$(window).on('load', function() { // makes sure the whole site is loaded 
+            $('#status').fadeOut(); // will first fade out the loading animation 
+            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+            $('body').delay(350).css({'overflow':'visible'});
+})
+
+//SMOOTH SCROLLING
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
